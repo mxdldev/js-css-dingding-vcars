9cb998376a8ea9610a4d8739b84c29c5a904cb90
geduo83 <geduo_83@163.com>
2020-04-07 19:36:58 +0800

    flex add

--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -30,6 +30,8 @@ if(AFAppX.registerApp) {
 function success() {
 require('../../app');
 require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/AdminCar/AdminCar?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/Me/Me?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }
