5b29ec609e57be8eaf2f0370f992c3b8acba19cb
geduo83 <geduo_83@163.com>
2020-04-10 11:01:12 +0800

    code perfect

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ App({
   onLaunch(options) {
     // 第一次打开
     // options.query == {number:1}
-    console.info('App onLaunch');
-    console.info('App onLaunch,corpId:' + options.query.corpId);
+    console.info('App onLaunch start...');
+    console.info('corpId:' + options.query.corpId);
     this.session.corpId = options.query.corpId;
-    console.info('session corpId:' + this.session.corpId);
   },
   onShow(options) {
-
     // 从后台被 scheme 重新打开
-    // options.query == {number:1}
-    console.info('onShow start...');
-
-   
+    console.info('App onShow start...');  
   },
   session: {
     corpId: '',
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,9 @@
 Page({
   onLoad(query) {
     // 页面加载
-    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
-    //获取免登录访问授权码
-    var that = this;
-    dd.getAuthCode({
-      success: function (res) {
-        console.info('authCode:' + res.authCode);
-        getApp().session.authCode = res.authCode;
-        that.getUserInfo();
-      },
-      fail: function (err) {
-      }
-    });
+    //console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+    console.info('Apply Page onLoad start...');
+    this.getAuthCode();
   },
   onReady() {
     // 页面加载完成
@@ -43,10 +34,29 @@ Page({
       path: 'pages/index/index',
     };
   },
+  getAuthCode() {
+    //获取免登录访问授权码
+    console.info('getAuthCode start...');
+    var that = this;
+    dd.getAuthCode({
+      success: function (res) {
+        console.info('getAuthCode success...');
+        console.info(JSON.stringify(res));
+        getApp().session.authCode = res.authCode;
+        that.getUserInfo();
+      },
+      fail: function (err) {
+        console.info("getAuthCode fail...");
+        console.info(JSON.stringify(err));
+      }
+    });
+  }
+  ,
   getUserInfo() {
     //获取用户ID
+    console.info('getUserInfo start...');
     var url = 'http://123.56.26.99:9050/UserInfo/GetUserInfoByAuthCode?authCode=' + getApp().session.authCode
-    var that=this;
+    var that = this;
     dd.httpRequest({
       headers: {
         "Content-Type": "application/json",
@@ -56,32 +66,29 @@ Page({
       timeout: 10000,
       method: 'GET',
       success: function (res) {
-        console.info('success start...');
-        console.info(res);
-
+        console.info('getUserInfo success...');
+        console.info(JSON.stringify(res));
         getApp().session.userId = res.data.UserId;
         getApp().session.IsAdmin = res.data.IsAdmin;
         getApp().session.Name = res.data.Name;
         getApp().session.IsSuperAdmin = res.data.IsSuperAdmin;
-        console.log('获取用户信息完毕..');
 
-        //获取车辆列表开始
         that.getListVehicle();
-        
 
       },
       fail: function (res) {
-        console.info('fail start...');
-        console.info(res);
+        console.info('getUserInfo fail...');
+        console.info(JSON.stringify(res));
 
       },
       complete: function (res) {
-        console.info('complete start...');
-        //console.info(res);
+        console.info('getUserInfo complete...');
       }
     });
   },
   getListVehicle() {
+    console.info('getListVehicle start...');
+    //获取车辆列表开始
     var url = 'http://123.56.26.99:9050/ApplyVehicle/GetVehicleUseableList?authCode=' + getApp().session.authCode
 
     dd.httpRequest({
@@ -95,18 +102,17 @@ Page({
       method: 'GET',
       dataType: 'text',
       success: function (res) {
-        console.info('success start...');
-        console.info(res);
+        console.info('getListVehicle success...');
+        console.info(JSON.stringify(res));
 
       },
       fail: function (res) {
-        console.info('fail start...');
-        console.info(res);
+        console.info('getListVehicle fail...');
+        console.info(JSON.stringify(res));
 
       },
       complete: function (res) {
-        console.info('complete start...');
-        //console.info(res);
+        console.info('getListVehicle complete...');
       }
     });
   },
